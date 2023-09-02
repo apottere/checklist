e1bcda21af5689edf3712947ffc4089e8d15275c
Andrew Potter <apottere@gmail.com>
2023-09-02 09:57:44 -0400

    Generate frontend

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+/* @refresh reload */
+import { render } from 'solid-js/web';
+
+import '@/index.css';
+import { App } from '@/App';
+
+const root = document.getElementById('root');
+if (!root) throw new Error('No root element!');
+
+render(() => <App />, root);
